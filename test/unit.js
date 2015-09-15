4af3774b7351bbeedce8dc193b09745c79eb7cab
guybedford <guybedford@gmail.com>
2015-09-15 18:55:18 +0200

    jspm 0.17 upgrades

--- test/unit.js
+++ test/unit.js
@@ -1,10 +1,43 @@
-var nodeConversion = require('../lib/node-conversion');
+var parseDependencies = require('../lib/npm').parseDependencies;
+var depsTransformer = require('../lib/deps-transformer');
+var fs = require('fs');
+
+function arrayEqual(a, b) {
+  return !a.some(function(val, index) {
+    return b.indexOf(val) != index;
+  });
+}
+
+function testDepsTransformer(name, expectedOutput) {
+  var source = fs.readFileSync('./fixtures/deps-transformer/' + name + '.js');
+  var output = depsTransformer(source.toString());
+
+  if (output.usesProcess !== expectedOutput.usesProcess)
+    throw 'Deps detection of ' + name + ' expected to find process ' + expectedOutput.usesProcess;
+
+  if (output.usesBuffer !== expectedOutput.usesBuffer)
+    throw 'Deps detection of ' + name + ' expected to find buffer ' + expectedOutput.usesBuffer;
+
+  if (!arrayEqual(output.requires.sort(), expectedOutput.requires.sort()))
+    throw 'Deps detection of ' + name + ' has requires ' + JSON.stringify(output.requires) + ', but expected ' + JSON.stringify(expectedOutput.requires);
+}
+
+testDepsTransformer('process-detection1', {
+  requires: ['./invariant'],
+  usesProcess: true,
+  usesBuffer: false
+});
+testDepsTransformer('process-detection2', {
+  requires: [],
+  usesProcess: true,
+  usesBuffer: false
+});
 
 
 function testDependency(name, value, expectedName, expectedValue) {
   var deps = {};
   deps[name] = value;
-  deps = nodeConversion.parseDependencies(deps);
+  deps = parseDependencies(deps);
 
   for (var p in deps) {
     if (p != expectedName)
@@ -36,5 +69,8 @@ testDependency('@scoped/react', '0.x', '@scoped/react', '@scoped/react@0');
 testDependency('@scoped/react', '>=0.12.0', '@scoped/react', '@scoped/react@*');
 
 testDependency('get-size', '>=1.1.4 <1.3', 'get-size', 'get-size@~1.2.0');
+
+
+
 
 console.log('Unit tests passed');
