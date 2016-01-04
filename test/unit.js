746e14c2419fbaefee9d0113212e290c2d9532cf
guybedford <guybedford@gmail.com>
2016-01-04 20:47:15 +0200

    version conversion adjustments, optionaDependency including again

--- test/unit.js
+++ test/unit.js
@@ -1,38 +1,5 @@
 var parseDependencies = require('../lib/npm').parseDependencies;
-var depsTransformer = require('../lib/deps-transformer');
 var fs = require('fs');
-
-function arrayEqual(a, b) {
-  return !a.some(function(val, index) {
-    return b.indexOf(val) != index;
-  });
-}
-
-function testDepsTransformer(name, expectedOutput) {
-  var source = fs.readFileSync('./fixtures/deps-transformer/' + name + '.js');
-  var output = depsTransformer(source.toString());
-
-  if (output.usesProcess !== expectedOutput.usesProcess)
-    throw 'Deps detection of ' + name + ' expected to find process ' + expectedOutput.usesProcess;
-
-  if (output.usesBuffer !== expectedOutput.usesBuffer)
-    throw 'Deps detection of ' + name + ' expected to find buffer ' + expectedOutput.usesBuffer;
-
-  if (!arrayEqual(output.requires.sort(), expectedOutput.requires.sort()))
-    throw 'Deps detection of ' + name + ' has requires ' + JSON.stringify(output.requires) + ', but expected ' + JSON.stringify(expectedOutput.requires);
-}
-
-testDepsTransformer('process-detection1', {
-  requires: ['./invariant'],
-  usesProcess: true,
-  usesBuffer: false
-});
-testDepsTransformer('process-detection2', {
-  requires: [],
-  usesProcess: true,
-  usesBuffer: false
-});
-
 
 function testDependency(name, value, expectedName, expectedValue) {
   var deps = {};
@@ -49,26 +16,28 @@ function testDependency(name, value, expectedName, expectedValue) {
 
 testDependency('react', '0.1.1', 'react', 'react@0.1.1');
 testDependency('react', '=0.1.1', 'react', 'react@0.1.1');
-testDependency('react', '<0.12', 'react', 'react@0.11.0');
-testDependency('react', '<0.12.0', 'react', 'react@0.11.0');
+testDependency('react', '<0.12', 'react', 'react@0.11');
+testDependency('react', '<0.12.0', 'react', 'react@0.11');
 testDependency('react', '~0.1.1', 'react', 'react@~0.1.1');
 testDependency('react', '^0.12', 'react', 'react@^0.12');
-testDependency('react', '0.12.x', 'react', 'react@~0.12.0');
+testDependency('react', '0.12', 'react', 'react@0.12');
+testDependency('react', '0.12.x', 'react', 'react@0.12');
 testDependency('react', '0.x', 'react', 'react@0');
 testDependency('react', '>=0.12.0', 'react', 'react@*');
+testDependency('react', '<2.3.4 >=2.3.3', 'react', 'react@2.3.3');
 
 // Scoped
 testDependency('@scoped/react', '0.11.0', '@scoped/react', '@scoped/react@0.11.0');
 testDependency('@scoped/react', '=0.11.0', '@scoped/react', '@scoped/react@0.11.0');
-testDependency('@scoped/react', '<0.12', '@scoped/react', '@scoped/react@0.11.0');
-testDependency('@scoped/react', '<0.12.0', '@scoped/react', '@scoped/react@0.11.0');
+testDependency('@scoped/react', '<0.12', '@scoped/react', '@scoped/react@0.11');
+testDependency('@scoped/react', '<0.12.0', '@scoped/react', '@scoped/react@0.11');
 testDependency('@scoped/react', '~0.1.1', '@scoped/react', '@scoped/react@~0.1.1');
 testDependency('@scoped/react', '^0.12', '@scoped/react', '@scoped/react@^0.12');
-testDependency('@scoped/react', '0.12.x', '@scoped/react', '@scoped/react@~0.12.0');
+testDependency('@scoped/react', '0.12.x', '@scoped/react', '@scoped/react@0.12');
 testDependency('@scoped/react', '0.x', '@scoped/react', '@scoped/react@0');
 testDependency('@scoped/react', '>=0.12.0', '@scoped/react', '@scoped/react@*');
 
-testDependency('get-size', '>=1.1.4 <1.3', 'get-size', 'get-size@~1.2.0');
+testDependency('get-size', '>=1.1.4 <1.3', 'get-size', 'get-size@1.2');
 
 
 
